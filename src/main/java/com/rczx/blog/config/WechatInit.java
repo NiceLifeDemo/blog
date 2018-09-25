6157b18ef088d53df27ae04558f72e7923a8ba6a
NiceLifeDemo <910249841@qq.com>
2018-09-25 11:05:08 +0800

    Initial commit
    Signed-off-by: NiceLifeDemo <910249841@qq.com>

--- src/main/java/com/rczx/blog/config/WechatInit.java
+++ src/main/java/com/rczx/blog/config/WechatInit.java
@@ -4,7 +4,9 @@ import com.rczx.blog.util.Common;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 @Slf4j
 public class WechatInit implements CommandLineRunner {
     @Autowired
@@ -15,7 +17,7 @@ public class WechatInit implements CommandLineRunner {
 
         log.info("初始化微信配置");
 
-        Common.WECHAT_URL=myConfig.getWechatUrl();
+        Common.WECHAT_REDIRECT_URL=myConfig.getWechatUrl();
 
         log.info("初始化微信配置完成");
 
